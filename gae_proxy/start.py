495c9d7516fc5f328c8f6d621a0c25577b8ef552
aOJzQT <aOJzQT@users.noreply.github.com>
2015-09-21 00:05:20 +0800

    fix #1079
    * set keep_running = True to make gae_proxy restart successfully

--- gae_proxy/start.py
+++ gae_proxy/start.py
@@ -11,10 +11,15 @@ if os.path.islink(__file__):
 current_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_path)
 import local.proxy as client
-
+import local.connect_control as connect_control
+import local.xlog as xlog
 
 def main():
     try:
+        xlog.debug("# TODO keep_running: %s", connect_control.keep_running)
+        # gae_proxy/local/proxy.py will check 'keep_running' looply
+        # if gae_proxy wants to be up, 'keep_running' should NOT be False
+        connect_control.keep_running = True
         client.main()
     except KeyboardInterrupt:
         sys.exit()
